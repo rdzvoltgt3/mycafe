96754de7d6201569772b24365c42b267aee0f908
rdzvoltgt3 <raavolt@gmail.com>
2026-06-02 06:39:14 +0700

    Implement Menu, Cart, and Checkout functionality dengan corresponding views dan routes

---
+++ app/Http/Controllers/MenuController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\support\Facades\Session;
+use Illuminate\Http\Request;
+use App\Models\Item;
+
+class MenuController extends Controller
+{
+    public function index(Request $request)
+    {
+        $tableNumber = $request->query('meja');
+        if ($tableNumber) {
+            Session::put('tableNumber', $tableNumber);
+        } 
+
+        $items = Item::where('is_active', 1)->orderBy('name', 'asc')->get();
+
+        return view('customer.menu', compact('items', 'tableNumber'));
+    }
+
+    public function cart()
+    {
+        $cart = Session::get('cart');
+        return view('customer.cart', compact('cart'));
+    }
+
+    public function addToCart(Request $request)
+    {
+        $menuId = $request->input('id');
+        $menu = Item::find($menuId);
+
+        if (!$menu) {
+            return response()->json([
+             'status' => 'error',
+             'message' => 'Menu tidak ada'
+             ]);
+        }
+        $cart = Session::get('cart');
+
+        if(isset($cart[$menuId])) {
+            $cart[$menuId]['qty'] += 1;
+        } else {
+            $cart[$menuId] = [
+                'id' => $menu->id,
+                'name' => $menu->name,
+                'price' => $menu->price,
+                'image' => $menu->image,
+                'qty' => 1
+            ];
+        }
+
+        Session::put('cart', $cart);
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Menu ditambahkan ke keranjang',
+            'cart' => $cart
+        ]);
+    }
+}
